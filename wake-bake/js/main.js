09bcb755bab55e6195913d8215e0aec8411c69d8
unenl1ghtened <margarita0rzkv@gmail.com>
2024-09-28 19:42:18 +0100

    Update main.js

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -38,4 +38,58 @@
         }
     }
 
+    //tabs
+
+    const tabsControl = document.querySelector('.tab-control');
+
+    tabsControl.addEventListener('click', toggleTab);
+
+    function toggleTab (e) {
+        const tabControl = e.target.closest('.tab-control__link'); 
+        if (!tabControl) return;
+        e.preventDefault();
+        if (tabControl.classList.contains('tab-control__link--active')) return;        if(tabControl.classList.contains('tab-control__link--active')) return;
+
+        const tabContentID = tabControl.getAttribute('href');
+        const tabContent = document.querySelector(tabContentID);
+        const activeControl = document.querySelector('.tab-control__link--active');
+        const activeContent = document.querySelector('.tab-content--show');
+
+        if (activeControl) {
+            activeControl.classList.remove('tab-control__link--active');
+        }
+
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show');
+        }
+
+        tabControl.classList.add('tab-control__link--active');
+        tabContent.classList.add('tab-content--show');
+    }
+
+    //accordion
+
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return;
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            };
+
+        });
+
+    });
+
 })();
